de4bf71a982ad2f230b18a3b5c7037f0ce6ee61d
angelDariaux <angeldariaux@gmail.com>
2022-07-27 19:30:15 -0600

    testeando el input

--- firstAutomation.py
+++ firstAutomation.py
@@ -1,1 +1,31 @@
+from time import sleep
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+import unittest
+
+
+class firstTest(unittest.TestCase):
+
+  def setUp(self):
+    self.driver = webdriver.Chrome(executable_path= './chromedriver')
+    driver = self.driver
+    driver.get('https://techstepacademy.com/training-ground')
+    # driver.find_element(By.CSS_SELECTOR, '#ipt1')
+
+  def test_one(self):
+    driver = self.driver
+
+    write_something = driver.find_element(By.CSS_SELECTOR, '#ipt1')
+    write_something.send_keys('Hola como estas???')
+
+    sleep(6)
+
+
+  def tearDown(self):
+    self.driver.close()
+
+
+if __name__ == "__main__":
+  unittest.main(verbosity= 2)
+
+
