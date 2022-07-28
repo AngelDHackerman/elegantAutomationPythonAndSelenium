4232d5bb1c2ca366fa603ccd2c6f6499905710c5
angelDariaux <angeldariaux@gmail.com>
2022-07-27 22:29:29 -0600

    automated the first section of the web page, input, button and alerts

--- alerts.py
+++ alerts.py
@@ -13,8 +13,31 @@ class alets_challenge(unittest.TestCase):
     driver = self.driver
     driver.get('https://techstepacademy.com/training-ground')
 
-  def test_alerts(self):
+
+      # ? First input field and first 2 buttons with alerts
+
+  def test_first_section(self):
     driver = self.driver
+    
+    first_input = driver.find_element(By.CSS_SELECTOR, '#ipt1')
+    first_input.send_keys('This is a testing for the input')
+    sleep(1)
+    first_input.clear()  # we clean the input 
+    sleep(1)
+
+    button1 = driver.find_element(By.CSS_SELECTOR, '#b1').click()
+    alert = driver.switch_to.alert  # move the focus of browser to the alert
+    sleep(1)
+    alert.accept()  # we accept the alert
+    sleep(1)
+
+    button2 = driver.find_element(By.CSS_SELECTOR, '#b2').click()
+    alert = driver.switch_to.alert  # move the focus of browser to the alert
+    sleep(1)
+    alert.accept()  # we accept the alert
+    sleep(1)
+
+    sleep(2)
 
 
   def tearDown(self):
